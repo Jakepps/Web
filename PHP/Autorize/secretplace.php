2e79712ca73c497aea2af3cb7aa6495b96148717
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-05-04 22:21:27 +0300

    добавил проверку на количество ошибок и 1 минуту

--- PHP/Autorize/secretplace.php
+++ PHP/Autorize/secretplace.php
@@ -2,7 +2,7 @@
     session_start();
     echo session_id();
     if(!isset($_SESSION['logged_user'])){
-        header("Location: index.php");
+        header("Location: login.php");
         exit;
     }
 ?>
@@ -16,5 +16,7 @@
 
     <body>
         <p>Привет, <?php echo $_SESSION['logged_user']; ?>, ты на секретной странице!!!</p>
+        <img src="image/cat.jpg" alt="Котя 1">
+        <img src="image/cat2.jpg" alt="Котя 2">
     </body>
 </html>
